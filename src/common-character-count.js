5f74d699f4b4265142c9fb8abc3c87240a8e6d86
tjgtll <volotji@gmail.com>
2022-10-17 22:43:36 +0300

    init: carbon and common tasks

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function getCommonCharacterCount(s1, s2) {
+	let result = 0;
+	let array1 = s1.split('');
+	let array2 = s2.split('');
+
+	for(let i = 0; i <= array1.length; i++) {
+		if(array2.indexOf(array1[i]) >= 0) {
+			let step = array2.indexOf(array1[i]);
+			result += 1;
+			array2.splice(step, 1)
+		}
+		}
+
+	return result;
 }
 
 module.exports = {
